c038ecf1ffcb7e73c58ca1d8496731a3e97d9f62
Maciej Siwek <me@maciejsiwek.pl>
2015-08-10 16:05:10 +0200

    Initial Commit A basic boilerplate for an app.

---
+++ containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WordsGame from './WordsGame';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WordsGame />
+    );
+  }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './containers/App';
+//ADD CSS HERE
+
+React.render(
+  <App />,
+  document.getElementById('root')
+);
