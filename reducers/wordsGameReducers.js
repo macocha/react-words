788688d7eb0ce82354935951fa0c517efba86fb8
Maciej Siwek <me@maciejsiwek.pl>
2015-08-13 16:04:48 +0200

    Added a code to generate chunks

--- reducers/wordsGameReducers.js
+++ reducers/wordsGameReducers.js
@@ -41,7 +41,62 @@ const initialState = {
 
 const scoreIncrement = 10;
 const clearBonus = 30;
+const numOfChunks = 20;
 
+//n is a number of chunks to split to
+function splitIntoChunks(word, n) {
+  if (n === undefined) {
+    if (word.length < 2)
+      throw 'somehow chunk got smaller than 2 chars';
+    else if (word.length === 2 || word.length === 3)
+      return [word]
+    else if (word.length === 4)
+      return [word.slice(0, 2), word.slice(2)]
+    else if (word.length < 7) {
+      let slicePoint = Math.floor(Math.random()*2) + 2;
+      return [word.slice(0, slicePoint)].concat(splitIntoChunks(word.slice(slicePoint)));
+    } else {
+      let slicePoint = Math.floor(Math.random()*3) + 2;
+      return [word.slice(0, slicePoint)].concat(splitIntoChunks(word.slice(slicePoint)));
+    }
+  } else {
+    if (n*2 > word.length || n <= 0)
+      throw `Can't do it sire.`;
+    else if (n == 1)
+      return [word]
+    else {
+      let slicePoint = Math.round(word.length/n);
+      return [word.slice(0, slicePoint)].concat(splitIntoChunks(word.slice(slicePoint), n-1));
+    }
+  }
+}
+
+export function prepareChunks(wordsList) {
+  let wordsToFit = 2;
+  let tries = 0;
+  wordsList.sort((a,b) => a.length - b.length);
+  //Sort array by word length, so we have longer words to fit in the end
+  //It tries to split the words a few times, just in case rngesus is not on our side.
+  while(++tries) {
+    try {
+      let chunks = [];
+      for (let i = 0; i < wordsList.length-wordsToFit; i++) {
+        chunks = chunks.concat(splitIntoChunks(wordsList[i]));
+      }
+      for (let i = wordsList.length - wordsToFit; i < wordsList.length; i++) {
+        chunks = chunks.concat(splitIntoChunks(wordsList[i], Math.floor((numOfChunks - chunks.length)/(wordsList.length - i))));
+      }
+      return chunks;
+    } catch (e) {
+      if (tries > 10) {
+        wordsToFit = 3;
+      }
+      if (tries > 20) {
+        return undefined;
+      }
+    }
+  }
+}
 
 export function wordsGame(state = initialState, action) {
   switch (action.type) {
